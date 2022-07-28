a70d1d334f2e3b39f7c550b48bca2a8083c469c3
Ricardo Silva <ricardo.silvamain@gmail.com>
2022-07-28 10:19:04 -0300

    600

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -9,7 +9,7 @@ import { map, Observable, tap } from 'rxjs';
 })
 export class PokeAPIService {
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100'
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=600'
 
   constructor(
     private http: HttpClient
